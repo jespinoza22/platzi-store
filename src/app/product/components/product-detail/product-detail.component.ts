48ec4633a5aa35762cfce901a9e575120e47e888
Junior Smits <jespinoza2292@gmail.com>
2020-05-08 20:53:21 -0500

    complete download files

--- src/app/product/components/product-detail/product-detail.component.ts
+++ src/app/product/components/product-detail/product-detail.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
 
+import * as FileSaver from 'file-saver';
+
 import { switchMap } from 'rxjs/operators';
 import { Observable } from 'rxjs';
 
@@ -83,6 +85,8 @@ export class ProductDetailComponent implements OnInit {
     this.productsService.getFile()
     .subscribe(content => {
       console.log(content);
+      const blob = new Blob([content], {type: 'text/plain;charset=utf-8'});
+      FileSaver.saveAs(blob, 'hello world.txt');
     });
   }
 }
