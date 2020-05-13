2cbd577bbd33711c83670ea6aa8d9d90d2381942
Junior Smits <jespinoza2292@gmail.com>
2020-05-13 09:30:09 -0500

    optimizing using change detection, onpush and pures pipes

--- src/app/contact/contact.module.ts
+++ src/app/contact/contact.module.ts
@@ -1,19 +1,24 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
-import { ContactComponent } from './components/contact/contact.component';
+import { MaterialModule } from './../material/material.module';
 
 import { ContactRoutingModule } from './contact-routing.module';
 import { SharedModule } from '@shared/shared.module';
+import { LayoutComponent } from './components/layout/layout.component';
+import { ListComponent } from './components/list/list.component';
 
 @NgModule({
   declarations: [
-    ContactComponent,
-  ],
+    LayoutComponent,
+    ListComponent],
   imports: [
     CommonModule,
     SharedModule,
-    ContactRoutingModule
+    ContactRoutingModule,
+    MaterialModule,
+    FormsModule
   ]
 })
 export class ContactModule {
